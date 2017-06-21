fc4908b8bd982662b9eae0b4c1c5aba11bf21275
Sergio Moreno <sergiomoreno@MacBook-Air-de-Sergio.local>
2017-06-22 01:04:03 +0200

    Login override

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller {
 
-    use AuthenticatesUsers;
+    #use AuthenticatesUsers;
+    use AuthenticatesUsers {
+        logout as performLogout;
+    }
     /**
      * Where to redirect users after login.
      *
@@ -20,10 +24,39 @@ class LoginController extends Controller {
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct(){
         $this->middleware('guest')->except('logout');
     }
-    
+
+    public function logout(Request $request){
+        $this->performLogout($request);
+        return redirect()->route('home');
+    }
+
+    public function login(Request $request) {
+        $this->validate($request, [
+            'email'    => 'required',
+            'password' => 'required',
+        ]);
+     
+        $login_type = filter_var($request->input('email'), FILTER_VALIDATE_EMAIL ) 
+            ? 'email' 
+            : 'nif';
+     
+        $request->merge([
+            $login_type => $request->input('email')
+        ]);
+     
+        if (Auth::attempt($request->only($login_type, 'password'))) {
+            return redirect()->intended($this->redirectPath());
+        }
+     
+        return redirect()->back()
+            ->withInput()
+            ->withErrors([
+                'email' => '¡Parece que no existes!',
+        ]); 
+    }
+
     
 }
